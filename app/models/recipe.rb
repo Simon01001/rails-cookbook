f8f39ddd1423d60e978f2a33e1ae073dc4ae36f9
szymon niedorezo <szymonniedorezo@gmail.com>
2024-09-30 11:46:12 +0100

    validations

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,8 @@
 class Recipe < ApplicationRecord
+  has_many :bookmarks
+  has_many :categories, through: :bookmarks
+
+  validates :description, :name, presence: true
+  validates :name, uniqueness: true
+  validates :rating, numericality: { in: 0..10 }
 end
